4b3a421987e532b9cadd8cabc0a26ff4a0827a2a
angeloMBRamos <angelo.mb.ramos@gmail.com>
2023-03-16 16:35:09 +0000

    Update Main.java

--- Ficha3/src/Main.java
+++ Ficha3/src/Main.java
@@ -1,11 +1,4 @@
-import java.util.Scanner;
 
-/*Faça um programa que leia um número inteiro (variável limite), um incremento (variável salto) e imprima os números inteiros de 0 até limite inclusive, com incremento de salto.
-        Suponha que limite e salto são maiores que zero.
-        Exemplo:
-        Valores lidos:
-        10 (limite) e 3 (salto)Saída do algoritmo: 0 3 6 9
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
