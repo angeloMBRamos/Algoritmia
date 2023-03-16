5c45c5b7f79bffd6689f8545724719804ba51d7f
angeloMBRamos <angelo.mb.ramos@gmail.com>
2023-03-16 16:56:31 +0000

    Update exercicio10.java

--- Ficha3/src/exercicio10.java
+++ Ficha3/src/exercicio10.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 /*Escreva um algoritmo que leia uma quantidade desconhecida de números inteiros
  positivos fornecida pelo utilizador e conte quantos deles estão nos seguintes
- intervalos: [0.25], [26,50], [51,75] e [76,100].
+ intervalos: [0,25], [26,50], [51,75] e [76,100].
  A entrada de dados deve terminar quando for lido um número negativo.
  */
 public class exercicio10 {
+    public static void main(String[] args) {
+
+    }
 }
