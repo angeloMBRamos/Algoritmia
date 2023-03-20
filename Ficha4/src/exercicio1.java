24200137f6608629d671574ac1e584a6337397d9
angeloMBRamos <angelo.mb.ramos@gmail.com>
2023-03-20 00:36:41 +0000

    exercice at home

---
+++ Ficha4/src/exercicio1.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Scanner;
+/*Implemente  um  programa  que  peça  ao  utilizador  dois  valores  e  um  carácter  representando
+uma das  quatro  operações aritméticas (+, -,  *,  /).  Apresente  o  resultado  de  aplicar a
+operação correspondente  aos  valores.  No  final, deverá  perguntar  ao  utilizador  se  deseja  repetir,
+permitindo-lhe efetuar novos cálculos.
+Exemplo:Deseja continuar? (introduza s/n).
+ */
+public class exercicio1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n1, n2;
+        String option;
+
+        System.out.println("Enter your a number");
+        n1 = input.nextInt();
+        System.out.println("Enter another number");
+        n2 = input.nextInt();
+
+
+    }
+}
