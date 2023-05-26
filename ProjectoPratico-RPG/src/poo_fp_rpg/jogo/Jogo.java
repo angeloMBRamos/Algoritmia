e2520f0f1b00fecf083e44d6b7d6ab19b5650085
angeloMBRamos <angelo.mb.ramos@gmail.com>
2023-05-26 01:10:51 +0100

    work at home

--- ProjectoPratico-RPG/src/poo_fp_rpg/jogo/Jogo.java
+++ ProjectoPratico-RPG/src/poo_fp_rpg/jogo/Jogo.java
@@ -1,9 +1,6 @@
 package poo_fp_rpg.jogo;
 
-import poo_fp_rpg.entidades.Cavaleiro;
-import poo_fp_rpg.entidades.Heroi;
-import poo_fp_rpg.entidades.NPC;
-import poo_fp_rpg.entidades.Vendedor;
+import poo_fp_rpg.entidades.*;
 import poo_fp_rpg.items.Arma;
 import poo_fp_rpg.items.ItemHeroi;
 import poo_fp_rpg.items.PocaoHP;
@@ -18,22 +15,43 @@ public class Jogo {
         ArrayList<Heroi> listaHerois = new ArrayList<Heroi>();
         ArrayList<PocaoHP> listaPocoes = new ArrayList<>();
 
-        Arma espada = new Arma("Espada", 50, listaHerois, 30);
+        Arma espada = new Arma("Espada", 2, listaHerois, 5);
+        Arma machado = new Arma("Machado", 3, listaHerois, 4);
+        Arma foice = new Arma("Nunchaku", 3, listaHerois, 3);
+        Arma arco = new Arma("Arco", 4, listaHerois, 5);
+        Arma fisga = new Arma("Fisga", 2, listaHerois, 3);
+        Arma besta = new Arma("Besta", 3,listaHerois, 4 );
+        Arma cuboMagico = new Arma("Cubo", 3, listaHerois, 4);
+        Arma estrelaMagica = new Arma("Estrela", 4, listaHerois, 5);
+        Arma fosforosMagicos = new Arma("Fósforos", 2, listaHerois, 3);
 
-        PocaoHP rejuvenescer = new PocaoHP("Rejuvenescer", 20, listaHerois, 50);
+        PocaoHP carlsberg = new PocaoHP("Carlsberg", 2, listaHerois, 25);
+        PocaoHP stout = new PocaoHP("Stout", 2, listaHerois,12 );
+        PocaoHP budweiser = new PocaoHP("Budweiser", 3, listaHerois, 15);
+        PocaoHP sb = new PocaoHP("SuperBock", 2, listaHerois, 20);
 
-        listaPocoes.add(rejuvenescer);
-
-        Cavaleiro hercules = new Cavaleiro("Hercules", 100, 300, 0, 500, listaPocoes, espada);
-        NPC bot = new NPC("Bot", 100, 50);
-
-        listaHerois.add(hercules);
+        Cavaleiro hercules = new Cavaleiro("Hercules", 0, 0, 0, 0, listaPocoes, espada);
+        Arqueiro flechas = new Arqueiro("Flechas", 0, 0, 0, 0, listaPocoes,arco);
+        Feiticeiro gandalf = new Feiticeiro("Gandalf", 0, 0, 0, 0, listaPocoes, cuboMagico);
+        NPC bot = new NPC("Bot", 100, 25);
 
         ArrayList<ItemHeroi> stockVendedor = new ArrayList<>();
         stockVendedor.add(espada);
-        stockVendedor.add(rejuvenescer);
+        stockVendedor.add(machado);
+        stockVendedor.add(foice);
+        stockVendedor.add(arco);
+        stockVendedor.add(fisga);
+        stockVendedor.add(besta);
+        stockVendedor.add(cuboMagico);
+        stockVendedor.add(estrelaMagica);
+        stockVendedor.add(fosforosMagicos);
+        stockVendedor.add(carlsberg);
+        stockVendedor.add(stout);
+        stockVendedor.add(budweiser);
+        stockVendedor.add(sb);
 
         Vendedor vendedor = new Vendedor(stockVendedor);
+        vendedor.imprimirInventário();
 
         Scanner input = new Scanner(System.in);
         int num;
@@ -42,33 +60,83 @@ public class Jogo {
                 System.out.println("Escolha a dificuldade do jogo: ");
                 System.out.println("1. Fácil\n2. Dificil\n3. Sair");
                 num = input.nextInt();
-
                 if (num < 1 || num > 3) {
-                    System.out.println("Please choose a valid option from the Menu");
-
+                    System.out.println("Escolha uma opção válida do menu");
                 }
+
             }while (num < 1 || num > 3) ;
 
                 switch (num) {
 
                     case 1:
+                        System.out.println("Escolha o seu Heroi");
+                        System.out.println("1. Hercules\n2. Flechas\n3. Gandalf");
+                        int escolhaFacil = input.nextInt();
+                        if(escolhaFacil == 1){
+                            System.out.println("O seu herói é o: " + hercules.getNome());
+                            System.out.println("Tem direito a 300 pontos.");
+                            hercules.setVida(150);
+                            hercules.setForca(25);
+                            hercules.setOuro(20);
+                            System.out.println("Aqui está o seu herói: ");
+                            hercules.mostrarDetalhes();
+
+                        } else if (escolhaFacil == 2) {
+                            System.out.println("O seu héroi é o " + flechas.getNome());
+                            System.out.println("Tem direito a 300 pontos.");
+                            flechas.setVida(150);
+                            flechas.setForca(25);
+                            flechas.setOuro(20);
+                            System.out.println("Aqui está o seu herói: ");
+                            flechas.mostrarDetalhes();
+                        } else if (escolhaFacil == 3) {
+                            System.out.println("O seu herói é o " + gandalf.getNome());
+                            System.out.println("Tem direito a 300 pontos.");
+                            gandalf.setVida(150);
+                            gandalf.setForca(25);
+                            gandalf.setOuro(20);
+                            System.out.println("Aqui está o seu herói: ");
+                            gandalf.mostrarDetalhes();
+                        }
                         break;
                     case 2:
+                        System.out.println("Escolha o seu Heroi");
+                        System.out.println("1. Hercules\n2. Flechas\n3. Gandalf");
+                        int escolhaDificil = input.nextInt();
+                        if(escolhaDificil == 1){
+                            System.out.println("O seu herói é o: " + hercules.getNome());
+                            System.out.println("Tem direito a 220 pontos.");
+                            hercules.setVida(110);
+                            hercules.setForca(22);
+                            hercules.setOuro(15);
+                            System.out.println("Aqui está o seu herói: ");
+                            hercules.mostrarDetalhes();
+                        } else if (escolhaDificil == 2) {
+                            System.out.println("O seu héroi é o " + flechas.getNome());
+                            System.out.println("Tem direito a 300 pontos.");
+                            flechas.setVida(110);
+                            flechas.setForca(22);
+                            flechas.setOuro(15);
+                            System.out.println("Aqui está o seu herói: ");
+                            flechas.mostrarDetalhes();
+                        } else if (escolhaDificil == 3) {
+                            System.out.println("O seu herói é o " + gandalf.getNome());
+                            System.out.println("Tem direito a 300 pontos.");
+                            gandalf.setVida(110);
+                            gandalf.setForca(22);
+                            gandalf.setOuro(15);
+                            System.out.println("Aqui está o seu herói: ");
+                            gandalf.mostrarDetalhes();
+                        }
                         break;
                 }
             } while (num != 3);
 
-            Scanner input = new Scanner(System.in);
-            int opcao;
+        hercules.mostrarDetalhes();
 
-            System.out.println("1.Fácil\n2.Dificil");
-            opcao = input.nextInt();
-            if (opcao == 1) {
-                System.out.println("Tem direito a 300 pontos.");
-                System.out.println("Quantos pontos de vida quer?");
+
 
             }
-        }
 
     public static void main(String[] args) {
 
@@ -79,7 +147,7 @@ public class Jogo {
 
 
 
-        hercules.mostrarDetalhes();
+        /*hercules.mostrarDetalhes();
         System.out.println();
         rejuvenescer.mostrarDetalhes();
         System.out.println();
@@ -91,7 +159,7 @@ public class Jogo {
         System.out.println();
         espada.mostrarDetalhes();
         System.out.println();
-        hercules.atacar(bot);
+        hercules.atacar(bot);*/
 
     }
 }
